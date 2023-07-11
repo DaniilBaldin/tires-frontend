dde6a856ad71b746b8da45b5a2097cc2c028b8bd
daniil <daniil_baldin@ukr.net>
2023-07-11 16:33:50 +0300

    First commit(Added basic pages, routing)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [svelte()],
+    output: 'static',
+    adapter: vercelStatic(),
 });
